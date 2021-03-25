1ebf1010767c0da2ddb588e3d1890092c3031203
Fausto Carcassi <fausto.carcassi@gmail.com>
2021-03-25 15:42:18 +0100

    Fix typo

--- neuralNetsLearning/serverJobs/runjob.sh
+++ neuralNetsLearning/serverJobs/runjob.sh
@@ -6,6 +6,6 @@
 module load 2020
 module load Python/3.8.2-GCCcore-9.3.0
 # Activate virtual environment
-source ../../../env/bin/activate
+source ../../../venv/bin/activate
 
 python -u ../simulation.py
