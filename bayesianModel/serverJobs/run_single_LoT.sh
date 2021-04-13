a709daa1bcd365d3b20b0688c5c4b8b99e833539
Fausto Carcassi <fausto.carcassi@gmail.com>
2021-04-13 11:21:05 +0200

    Update run_single_LoT.sh

--- bayesianModel/serverJobs/run_single_LoT.sh
+++ bayesianModel/serverJobs/run_single_LoT.sh
@@ -8,4 +8,8 @@ module load Python/3.8.2-GCCcore-9.3.0
 # activate virtual environment
 source ../../../venv/bin/activate
 
-python -u ../model_fitting.py --sampler SMC --indexLoT $SLURM_ARRAY_TASK_ID
+python -u ../model_fitting.py \
+--sampler SMC \
+--indexLoT $SLURM_ARRAY_TASK_ID \
+--path_learningdata '../../data/learning_costs.pkl' \
+--path_L '../../data/lengths_data.npy'
