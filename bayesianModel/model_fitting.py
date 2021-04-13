3ed44de46cb7910c65fa152cedaa47da3cd8795e
Fausto Carcassi <fausto.carcassi@gmail.com>
2021-04-13 12:49:38 +0200

    Change model fitting

--- bayesianModel/model_fitting.py
+++ bayesianModel/model_fitting.py
@@ -91,9 +91,9 @@ def fit_variational(model, filename):
 def sample_smc(model, filename):
     with model:
         trace_smc = pm.sample_smc(
-            n_steps=1000, 
+            n_steps=500, 
             chains=4,
-            parallel=False
+            cores=1
         )
     # trace = az.from_pymc3(trace_smc, model=model)
     
