6f28fbf03d76ed77e8a605a0feb6d0269564b539
Fausto Carcassi <fausto.carcassi@gmail.com>
2021-04-15 10:39:43 +0200

    Update plotting.py

--- bayesianModel/plotting.py
+++ bayesianModel/plotting.py
@@ -39,14 +39,13 @@ def plot_data_fitted(trace, LoT, category_i, outcome_i, ax=None):
     return fig, ax
     
     
-def plot_all_in_folder(fglob='serverJobs/*.xz'):
+def plot_all_in_folder(fglob, path_L, path_learningdata):
     
-    L, category_i, cost_i = get_data(
-        path_L='../../data/lengths_data.npy', 
-        path_learningdata='../../data/learning_costs.pkl'
-    )
+    L, category_i, cost_i = get_data(path_L, path_learningdata)
     L_extended, effective_LoT_indices = get_extended_L_and_effective(L)
+    print('Starting to plot')
     for fpath in glob(fglob):
+        print('f: ', fpath)
         fname = path.splitext(path.basename(fpath))[0] 
         params = dict(s.split('-') for s in fname.split('_'))
         with lzma.open(fpath, 'rb') as f:
@@ -57,4 +56,10 @@ def plot_all_in_folder(fglob='serverJobs/*.xz'):
         
 
 if __name__=='__main__':
-    plot_all_in_folder()
+    # These paths are for the server, 
+    # i.e. from the point of view of serverJobs
+    plot_all_in_folder(
+        fglob='*.xz',
+        path_L='../../data/lengths_data.npy', 
+        path_learningdata='../../data/learning_costs.pkl'
+    )
